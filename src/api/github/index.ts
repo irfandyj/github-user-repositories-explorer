4423d4b8e9faeef823acf37f31d4f43ad1c49854
Jip Irfandy <irfandyjip98@gmail.com>
2025-10-13 22:19:12 +0700

    feat: install rhf and zod for forms

--- src/api/github/index.ts
+++ src/api/github/index.ts
@@ -1,9 +1,11 @@
 import { createAlova } from 'alova';
 import fetchAdapter from 'alova/fetch';
 import { createApis, withConfigType, mountApis } from './createApis';
+import ReactHook from 'alova/react';
 
 export const alovaInstance = createAlova({
   baseURL: 'https://api.github.com',
+  statesHook: ReactHook,
   requestAdapter: fetchAdapter(),
   beforeRequest: method => {},
   responded: res => {
