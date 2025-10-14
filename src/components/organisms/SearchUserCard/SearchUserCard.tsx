ba65a3167b0939fac1a14f3a0a146ed4c1ec8195
Jip Irfandy <irfandyjip98@gmail.com>
2025-10-14 16:10:39 +0700

    feat(components): make responsive scroll area for user display

--- src/components/organisms/SearchUserCard/SearchUserCard.tsx
+++ src/components/organisms/SearchUserCard/SearchUserCard.tsx
@@ -1,11 +1,13 @@
 import SearchUserForm from "@/components/molecules/SearchUserForm";
 import type { searchUserFormSchema } from "@/components/molecules/SearchUserForm/SearchUserForm.schema";
 import Apis from "@/api/github";
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { Card, CardContent } from "@/components/ui/card";
 import { FieldLegend, FieldSet } from "@/components/ui/field";
 import { useRequest } from "alova/client";
 import * as z from 'zod';
 import { cn } from "@/lib/utils";
+import { useScrollAreaHeight } from "@/hooks/useScrollAreaHeight";
 
 function searchGitHubUser(username: string) {
   return Apis.search.searchUsers({
@@ -30,17 +32,6 @@ export default function SearchUserCard({ className }: SearchUserCardProps) {
     // submit data function
     send,
 
-    // update form item
-    update,
-
-    // Submit successful callback binding
-    onSuccess,
-
-    // Submit failure callback binding
-    onError,
-
-    // Submit completed callback binding
-    onComplete
   } = useRequest(searchGitHubUser, {
     immediate: false,
     initialData: {
@@ -50,26 +41,35 @@ export default function SearchUserCard({ className }: SearchUserCardProps) {
     },
   })
 
+  const {
+    ref: scrollAreaRef,
+    height: scrollAreaHeight,
+  } = useScrollAreaHeight({ bottomGap: 16 })
+
   function onSubmit(data: z.infer<typeof searchUserFormSchema>) {
     send(data.username)
   }
 
   return (
     <Card className={cn("w-full max-w-md overflow-hidden", className)}>
-      <CardContent className="flex flex-col gap-4">
-        <FieldSet>
+      <CardContent className="flex flex-col gap-4 px-0">
+        <FieldSet className="px-6">
           <FieldLegend>GitHub User Repositories Explorer</FieldLegend>
           <SearchUserForm loading={loading} onSubmit={onSubmit} />
         </FieldSet>
 
         {data.items.length > 0 && (
-          <div className=''>
+          <ScrollArea
+            ref={scrollAreaRef}
+            className='px-6'
+            style={{ height: scrollAreaHeight || undefined }}
+          >
             <ul className="list-disc list-inside">
               {data.items.map((user) => (
                 <li key={user.id}>{user.login}</li>
               ))}
             </ul>
-          </div>
+          </ScrollArea>
         )}
 
       </CardContent>
