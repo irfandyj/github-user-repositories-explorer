63b7e420d90653c7980969930c817264148eedd4
Jip Irfandy <irfandyjip98@gmail.com>
2025-10-14 18:44:14 +0700

    feat(components): add user repositories display

--- src/components/organisms/SearchUserCard/SearchUserCard.tsx
+++ src/components/organisms/SearchUserCard/SearchUserCard.tsx
@@ -2,7 +2,7 @@ import SearchUserForm from "@/components/molecules/SearchUserForm";
 import type { searchUserFormSchema } from "@/components/molecules/SearchUserForm/SearchUserForm.schema";
 import Apis from "@/api/github";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FieldLegend, FieldSet } from "@/components/ui/field";
 import {
   Accordion,
@@ -16,11 +16,25 @@ import { cn } from "@/lib/utils";
 import { useMemo } from "react";
 import useResponsiveParentHeight from "@/hooks/useResponsiveParentHeight";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ExternalLink, Loader2, Star } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 function searchGitHubUser(username: string) {
   return Apis.search.searchUsers({
     params: {
       q: username,
+    }
+  })
+}
+
+function getReposByUser(username: string) {
+  return Apis.repos.reposListForUser({
+    pathParams: {
+      username,
+    },
+    params: {
+      per_page: 100,
+      page: 1
     }
   })
 }
@@ -50,12 +64,25 @@ export default function SearchUserCard({ className }: SearchUserCardProps) {
     },
   })
 
+  const {
+    loading: getReposByUserLoading,
+    data: getReposByUserResponse,
+    send: getReposByUserSend,
+  } = useRequest(getReposByUser, {
+    immediate: false,
+    initialData: [],
+  })
+
   const displayedUsers = useMemo(() => {
     return searchUserResponse.items.slice(0, 5)
   }, [searchUserResponse.items])
 
   function onSubmit(data: z.infer<typeof searchUserFormSchema>) {
     searchUserSend(data.username)
+  }
+
+  function onAccordionTriggerClick(username: string) {
+    getReposByUserSend(username)
   }
 
   return (
@@ -69,12 +96,20 @@ export default function SearchUserCard({ className }: SearchUserCardProps) {
         <div className="SearchUserCard__search-result flex-1 border-t border-b min-h-0">
           <ScrollArea
             ref={scrollAreaRef}
-            className="h-full"
+            className="bg-background h-full"
           >
             <Accordion type="single" collapsible>
               {displayedUsers.map((user) => (
                 <AccordionItem key={user.id} value={user.id.toString()}>
-                  <AccordionTrigger className="flex items-center gap-2 px-6">
+                  <AccordionTrigger
+                    className="flex items-center gap-2 px-6"
+                    onClick={(e) => {
+                      const isOpen = e.currentTarget.getAttribute('data-state') !== 'open'
+                      if (isOpen) {
+                        onAccordionTriggerClick(user.login)
+                      }
+                    }}
+                  >
                     <Avatar>
                       <AvatarImage src={user.avatar_url} />
                       <AvatarFallback>{user.login.charAt(0)}</AvatarFallback>
@@ -83,13 +118,69 @@ export default function SearchUserCard({ className }: SearchUserCardProps) {
                       href={user.html_url}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="!text-muted-foreground"
+                      className="
+                        text-muted-foreground
+                        hover:underline
+                        hover:text-primary
+                        flex items-center gap-1
+                        group
+                      "
                     >
                       {user.login}
+                      <ExternalLink
+                        size={16}
+                        className="hidden
+                          group-hover:block group-hover:text-primary
+                        "
+                      />
                     </a>
                   </AccordionTrigger>
                   <AccordionContent className="px-6">
-                    {user.html_url}
+                    {getReposByUserLoading ?
+                      <Loader2 className="animate-spin" /> : (
+                        <div className="flex flex-col gap-2">
+                          {getReposByUserResponse.length > 0 ?
+                            getReposByUserResponse.map((repo) => (
+                              <Card key={repo.id} className="flex flex-col gap-2">
+                                <CardHeader className="flex justify-between items-center">
+                                  <CardTitle>
+                                    <a
+                                      href={repo.html_url}
+                                      target="_blank"
+                                      rel="noopener noreferrer"
+                                      className="
+                                        text-muted-foreground
+                                        hover:underline
+                                        hover:text-primary
+                                        flex items-center gap-1
+                                        group
+                                      "
+                                    >
+                                      {repo.name}
+
+                                      <ExternalLink
+                                        size={16}
+                                        className="hidden
+                                          group-hover:block group-hover:text-primary
+                                        "
+                                      />
+                                    </a>
+                                  </CardTitle>
+                                  <Badge variant="secondary">
+                                    {repo.stargazers_count}
+                                    <Star size={16} />
+                                  </Badge>
+                                </CardHeader>
+                                <CardContent>
+                                  <p className="text-sm text-muted-foreground">
+                                    {repo.description}
+                                  </p>
+                                </CardContent>
+                              </Card>
+                            )) :
+                            <div className="text-sm text-muted-foreground">No repositories found</div>}
+                        </div>
+                      )}
                   </AccordionContent>
                 </AccordionItem>
               ))}
